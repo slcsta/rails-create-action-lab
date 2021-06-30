4fc76c315c35d52402c2b2a9d881d3bf6ad9d02e
Sarah <sarahlchavez@gmail.com>
2021-06-30 11:19:59 -0700

    commit tree 7b6ea2620be0ac819380d0bc97b8e7e32fd84d17

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,25 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
   end
-
 end
+
+
+# def create
+#   @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+#   redirect_to student_path(@student)
+# end 
+
+# def show
+#   @student = Student.find(params[:id])
+# end 
+
